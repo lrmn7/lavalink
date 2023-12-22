948d23bf6f916da3005ebdec005d082f2cf525b9
lrmn7 <lrmn.dev@gmail.com>
2023-12-22 18:59:28 +0700

    🐛

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 # Experiment: build_local.sh
 #!/bin/bash
 echo Getting version info from lavalink-devs/Lavalink
-URL="https://api.github.com/repos/lavalink-devs/Lavalink/releases"
+URL="https://api.github.com/repos/Cog-Creators/Lavalink-Jars/releases"
 TAG=$(echo $(curl $URL -s) | jq .[0].tag_name)
 
 DOCKER_TAG="mewwme/lavalink:$(echo $TAG | tr -d '"')"
