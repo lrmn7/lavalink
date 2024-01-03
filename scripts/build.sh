c156909da083b00c8d658f1b0bbed0790bfd2213
L RMN <97253349+lrmn7@users.noreply.github.com>
2024-01-03 15:47:49 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 # Experiment: build_local.sh
 #!/bin/bash
 echo Getting version info from lavalink-devs/Lavalink
-URL="https://api.github.com/repos/Cog-Creators/Lavalink-Jars/releases"
+URL="https://github.com/lavalink-devs/Lavalink/releases/download/4.0.0/Lavalink.jar"
 TAG=$(echo $(curl $URL -s) | jq .[0].tag_name)
 
 DOCKER_TAG="mewwme/lavalink:$(echo $TAG | tr -d '"')"
